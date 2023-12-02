2ea805b1b78f37f027887ae06f662a1c49c1caeb
Muhu-Muza <schumannivan@gmail.com>
2023-12-02 09:29:41 +0300

    added custom param decorator

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -8,7 +8,7 @@ import { PrismaService } from "src/prisma/prisma.service";
 export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
   constructor(
     config: ConfigService,
-    private prisma: PrismaService
+    private prisma: PrismaService,
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
